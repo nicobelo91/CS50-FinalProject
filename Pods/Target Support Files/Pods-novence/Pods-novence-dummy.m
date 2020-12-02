5420f2bdbc49418f3f8bad7d50d1ea817e242177
Nico Cobelo <ingcobelo@gmail.com>
2020-12-02 14:24:37 -0300

    Add expirationDate when executing addButtonPressed

---
+++ Pods/Target Support Files/Pods-novence/Pods-novence-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_novence : NSObject
+@end
+@implementation PodsDummy_Pods_novence
+@end
--- novence/Controller/ListViewController.swift
+++ novence/Controller/ListViewController.swift
@@ -11,6 +11,8 @@ import CoreData
 class ListViewController: UITableViewController {
 
     @IBOutlet var productsList: UITableView!
+    var datePicker: UIDatePicker = UIDatePicker()
+    let toolBar = UIToolbar()
     var products = [Product]()
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
@@ -39,6 +41,11 @@ class ListViewController: UITableViewController {
         
         cell.productName.text = product.name
         
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd-MM-yyyy"
+        let formatteddate = formatter.string(from: product.expiration ?? Date(timeIntervalSinceReferenceDate: 0))
+        cell.expirationDate.text = formatteddate
+        
         return cell
     }
     
@@ -65,31 +72,91 @@ class ListViewController: UITableViewController {
 // MARK: - Add New Categories
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
         var textField = UITextField()
         
-        let alert = UIAlertController(title: "Create New Category", message: "", preferredStyle: .alert)
+        let alert = UIAlertController(title: "Add new product", message: "", preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Add", style: .default) { (action) in
             let newProduct = Product(context: self.context)
             newProduct.name = textField.text
+            newProduct.expiration = self.datePicker.date
             
             self.products.append(newProduct)
             self.saveProducts()
         }
         
         alert.addTextField { (alertTextField) in
-            alertTextField.placeholder = "Create new category"
+            alertTextField.placeholder = "Create new product"
             textField = alertTextField
+            self.doDatePicker()
+            alertTextField.inputView = self.datePicker
+            alertTextField.inputAccessoryView = self.toolBar
         }
         
+        //Grab the value from the text field and print it when the user clicks okay
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
+//        var textField = UITextField()
+//
+//        let alert = UIAlertController(title: "Create New Category", message: "", preferredStyle: .alert)
+//
+//        let action = UIAlertAction(title: "Add", style: .default) { (action) in
+//            let newProduct = Product(context: self.context)
+//            newProduct.name = textField.text
+//
+//            self.products.append(newProduct)
+//            self.saveProducts()
+//        }
+//
+//        alert.addTextField { (alertTextField) in
+//            alertTextField.placeholder = "Create new category"
+//            textField = alertTextField
+//        }
+//
+//        alert.addAction(action)
+//
+//        present(alert, animated: true, completion: nil)
     }
     
 // MARK: - Tableview Delegate Methods
     
-
+    func doDatePicker() {
+        self.datePicker = UIDatePicker(frame: CGRect(x: 0, y: self.view.frame.size.height - 220, width: self.view.frame.size.width, height: 216))
+        self.datePicker.backgroundColor = UIColor.white
+        datePicker.datePickerMode = .date
+        
+        //toolbar
+        
+        toolBar.barStyle = .default
+        toolBar.isTranslucent = true
+        toolBar.tintColor = UIColor(red: 92/255, green: 216/255, blue: 255/255, alpha: 1)
+        toolBar.sizeToFit()
+        
+        //Adding button toolbar
+        
+        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(doneClick))
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(cancelClick))
+        toolBar.setItems(([cancelButton,spaceButton,doneButton]), animated: true)
+        toolBar.isUserInteractionEnabled = true
+        
+    }
+    
+    @objc func doneClick() {
+        let dateFormatter1 = DateFormatter()
+        dateFormatter1.dateStyle = .medium
+        dateFormatter1.timeStyle = .none
+        
+        datePicker.isHidden = true
+        self.toolBar.isHidden = true
+    }
+    
+    @objc func cancelClick() {
+        datePicker.isHidden = true
+        self.toolBar.isHidden = true
+    }
     
 }
 
